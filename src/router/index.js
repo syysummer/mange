0e4a2138d8d2035d3854aacb7a956700d0a78c1a
sunyangyang <syysummer@yeah.net>
2020-11-15 21:38:24 +0800

    Signed-off-by: sunyangyang <syysummer@yeah.net>

--- src/router/index.js
+++ src/router/index.js
@@ -14,27 +14,32 @@ const routes = [
         path: '/index',
         name: 'Home',
         component: () => import(/* webpackChunkName: "login" */ '../views/layout/home/index.vue'),
+        meta:{title:'首页'},
       },
       {
         path: '/stats',
         name: 'Stats',
         component: () => import(/* webpackChunkName: "login" */ '../views/layout/stats/index.vue'),
+        meta:{title:'数据统计'},
       },
       {
         path: '/wms',
         name: 'WMS',
         component: () => import(/* webpackChunkName: "login" */ '../views/layout/wms/index.vue'),
         redirect: '/list',
+        meta:{title:'信息管理'},
         children:[
           {
             path: '/list',
             name: 'List',
             component: () => import(/* webpackChunkName: "login" */ '../views/layout/wms/list.vue'),
+            meta:{title:'列表展示'},
           },
           {
             path: '/users',
             name: 'User',
             component: () => import(/* webpackChunkName: "login" */ '../views/layout/wms/user.vue'),
+            meta:{title:'用户统计'},
           }
         ]
       },
--- src/views/layout/breadCrumbs.vue
+++ src/views/layout/breadCrumbs.vue
@@ -1,16 +1,36 @@
 <template>
-  <div class="breadcrumbs">
-    面包屑
-  </div>
+  <ul class="breadcrumbs">
+    <li v-for="(item,index) in list" :key="index">
+      <router-link :to="item.path">{{item.meta.title}}</router-link>
+      <span v-if="index != 0 && index < list.length - 1">/</span>
+    </li>
+  </ul>
 </template>
 
 <script>
-import { reactive, computed, toRefs } from "@vue/composition-api";
+import { reactive, computed, toRefs, onMounted, watch } from "@vue/composition-api";
 export default {
   setup(props,{root, refs, emit}){
+    const state = reactive({
+      list:[]
+    })
+    onMounted(() => {
+      let matched = root.$route.matched;
+      state.list = matched;
+    })
+    watch(() => root.$route, (to,from) => {
+      let matched = to.matched;
+      state.list = matched;
+    })
+    return {
+      ...toRefs(state)
+    }
   }
 }
 </script>
 <style lang="less" scoped>
- 
+@import '@/assets/css/common.less'; 
+  .breadcrumbs{
+    .flexBox(flex-start, center);
+  }
 </style>
